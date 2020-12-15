73e3633601e471a7096256c0dd1ee6e9050442a8
lindseyms <51429410+lindseyms@users.noreply.github.com>
2020-12-14 18:50:07 -0600

    Added shuttle status features

--- script.js
+++ script.js
@@ -17,17 +17,43 @@ window.addEventListener("load", function(){
    let copilotName = document.getElementById("copilotName");
    let fuelLevel = document.getElementById("fuelLevel");
    let cargoMass = document.getElementById("cargoMass");
-   
+   let faultyItems = document.getElementById("faultyItems");
+   let fuelStatus = document.getElementById("fuelStatus");
+   let launchStatus = document.getElementById("launchStatus");
+   let cargoStatus = document.getElementById("cargoStatus");
 
    form.addEventListener("submit", function(event){
       if(pilotName.value === "" || copilotName.value === "" || fuelLevel.value === "" || cargoMass.value === ""){
          alert("All fields are required!");
-         event.preventDefault();
       }
-      else if(!isNaN(pilotName.value) || !isNaN(copilotName.value) || isNaN(Number(fuelLevel.value)) || isNaN(Number(cargoMass.value))){
+      else if(isNaN(pilotName.value) && isNaN(copilotName.value) && !isNaN(Number(fuelLevel.value)) && !isNaN(Number(cargoMass.value))){
+
+         if(Number(fuelLevel.value) < 10000 || Number(cargoMass.value) > 10000){
+            faultyItems.style.visibility = "visible";
+            launchStatus.style.color = "red";
+            launchStatus.innerHTML = "Shuttle not ready for launch";
+            
+            if(Number(fuelLevel.value) < 10000){
+               fuelStatus.innerHTML = "Fuel level too low for launch";
+            }
+            if(Number(cargoMass.value) > 10000){
+               cargoStatus.innerHTML = "Cargo mass too great for shuttle to launch";
+            }
+         }
+
+         else{
+            launchStatus.style.color = "green";
+            launchStatus.innerHTML = "Shuttle is ready for launch";
+            faultyItems.style.visibility = "visible";
+            fuelStatus.innerHTML = "Fuel level high enough for launch";
+            cargoStatus.innerHTML = "Cargo mass low enough for launch";
+         }
+      }
+      else{
          alert("Make sure to enter valid information for each field!");
-         event.preventDefault();
       }
+      event.preventDefault();
+
    });
     
 });
