b2cf0b02bc2c5adfcca67917699ec20f934e25f6
lindseyms <51429410+lindseyms@users.noreply.github.com>
2020-12-14 19:53:32 -0600

    Added pilot and co-pilot status. Implemented get request to json

--- script.js
+++ script.js
@@ -12,6 +12,24 @@
 <img src="${}">
 */
 window.addEventListener("load", function(){
+   fetch("https://handlers.education.launchcode.org/static/planets.json").then(function(response){
+      response.json().then(function(json){
+         let planetSelector = Math.floor(Math.random() * Math.floor(json.length));
+
+         missionTarget.innerHTML = `
+            <h2>Mission Destination</h2>
+            <ol>
+               <li>Name: ${json[planetSelector].name}</li>
+               <li>Diameter: ${json[planetSelector].diameter}</li>
+               <li>Star: ${json[planetSelector].star}</li>
+               <li>Distance from Earth: ${json[planetSelector].distance}</li>
+               <li>Number of Moons: ${json[planetSelector].moons}</li>
+            </ol>
+            <img src="${json[planetSelector].image}">
+         `;
+      });
+   });
+
    let form = document.querySelector("form");
    let pilotName = document.getElementById("pilotName");
    let copilotName = document.getElementById("copilotName");
@@ -21,12 +39,18 @@ window.addEventListener("load", function(){
    let fuelStatus = document.getElementById("fuelStatus");
    let launchStatus = document.getElementById("launchStatus");
    let cargoStatus = document.getElementById("cargoStatus");
+   let missionTarget = document.getElementById("missionTarget");
+   let pilotStatus = document.getElementById("pilotStatus");
+   let copilotStatus = document.getElementById("copilotStatus");
 
    form.addEventListener("submit", function(event){
+
       if(pilotName.value === "" || copilotName.value === "" || fuelLevel.value === "" || cargoMass.value === ""){
          alert("All fields are required!");
       }
       else if(isNaN(pilotName.value) && isNaN(copilotName.value) && !isNaN(Number(fuelLevel.value)) && !isNaN(Number(cargoMass.value))){
+         pilotStatus.innerText = `Pilot ${pilotName.value} is ready for launch`;
+         copilotStatus.innerText = `Co-pilot ${copilotName.value} is ready for launch`;
 
          if(Number(fuelLevel.value) < 10000 || Number(cargoMass.value) > 10000){
             faultyItems.style.visibility = "visible";
